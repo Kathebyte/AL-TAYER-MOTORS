971c9f9cbdd2ac8ab7e4984f8f72c7d066f15f9d
Katherine Benjumea <kathe07338@gmail.com>
2023-01-03 11:37:29 +0400

    completed

--- Main.java
+++ Main.java
@@ -3,11 +3,11 @@ public class Main {
     public static void main(String[] args) {
 
 
-        // Usuario usuario = new Usuario("Katherine Benjumea", "0521758899", 0);
+        // Usuario usuario = new Usuario("Katherine Benjumea", "058745154", 0);
         // usuario.PrintInformation();
 
 
-        // Usuario usuario1 = new Usuario("Cristian Gutierrez", "971524587", 0);
+        // Usuario usuario1 = new Usuario("Cristian Gutierrez", "9715874598", 0);
         // usuario1.PrintInformation();
 
 
@@ -17,7 +17,7 @@ public class Main {
         // vehicule.getWarrantyTime();
 
 
-        Sale sale1= new Sale(new Usuario("Katherine Benjumea ", "0521785599", 1),new Employee("Cristian Gutierrez", 0101), new Vehicule("Ford", "Escape", 12000, "Blue", 25000, "SV", 5),1500,("15,12,2022"));
+        Sale sale1= new Sale(new Usuario("Katherine Benjumea ", "0587125874", 1),new Employee("Cristian Gutierrez", 0101), new Vehicule("Ford", "Escape", 12000, "Blue", 25000, "SV", 5),1500,("15,12,2022"));
         sale1.PrintAllInformation();
 
         
